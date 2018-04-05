2a408d489431f67d8e9ba9351ef10d6ecf536f5b
AhmedHassanHelal <ahmedhassan96@windowslive.com>
2018-04-05 19:59:36 +0200

    Update send_goals.py

--- send_goals.py
+++ send_goals.py
@@ -5,7 +5,7 @@ import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
-def movebase_client():
+def movebase_client(x, y):
 
     client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
     client.wait_for_server()
@@ -13,8 +13,8 @@ def movebase_client():
     goal = MoveBaseGoal()
     goal.target_pose.header.frame_id = "map"
     goal.target_pose.header.stamp = rospy.Time.now()
-    goal.target_pose.pose.position.x = 9.55
-    goal.target_pose.pose.position.y = 8.55	
+    goal.target_pose.pose.position.x = x
+    goal.target_pose.pose.position.y = y	
     goal.target_pose.pose.orientation.w = 1.0
 
     client.send_goal(goal)
@@ -25,11 +25,28 @@ def movebase_client():
     else:
         return client.get_result()
 
+
 if __name__ == '__main__':
     try:
+        # Initializes a rospy node to let the SimpleActionClient publish and subscribe
         rospy.init_node('movebase_client_py')
-        result = movebase_client()
-        if result:
-            rospy.loginfo("Goal execution done!")
+        x = 0.9
+        y = 0.9
+        for i in range(12):
+            for j in range(25):
+                if x > 0.7 and x < 9.34 and y > 0.7 and y < 9.34 :
+                    result = movebase_client(x, y)
+                    if result:
+                        rospy.loginfo("Goal execution done!")
+                    x = x+0.4
+            y = y+0.4
+            for j in range(25):
+                if x>0.7 and x<9.34 and y>0.7 and y<9.34 :
+                    result = movebase_client(x, y)
+                    if result:
+                        rospy.loginfo("Goal execution done!")
+                    x = x - 0.4
+            y = y + 0.4
+
     except rospy.ROSInterruptException:
         rospy.loginfo("Navigation test finished.")
